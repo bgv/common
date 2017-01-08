6e0787756b685ee68753b22d8c866314004e6004
Boris V <boris@codechimp.io>
2017-01-08 15:59:40 +0000

    Experimental separation of per route and combined stats. Per route can be quite heavy on service with many routes.

--- middleware/promstats.go
+++ middleware/promstats.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	mw "github.com/pressly/chi/middleware"
@@ -25,6 +26,61 @@ func PrometheusHandler() http.Handler {
 
 // PrometheusStats returns a new Prometheus middleware handler.
 func PrometheusStats(name string, buckets ...float64) func(http.Handler) http.Handler {
+
+	prometheus.Unregister(prometheus.NewProcessCollector(os.Getpid(), ""))
+	prometheus.Unregister(prometheus.NewGoCollector())
+	prometheus.MustRegister(prometheus.NewProcessCollector(os.Getpid(), name))
+	prometheus.MustRegister(prometheus.NewGoCollector())
+
+	requests := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace:   name,
+			Subsystem:   "http",
+			Name:        reqsName,
+			Help:        "How many HTTP requests processed, partitioned by status code, method and HTTP path.",
+			ConstLabels: prometheus.Labels{"service": name},
+		},
+		[]string{"code", "method"},
+	)
+	prometheus.MustRegister(requests)
+
+	if len(buckets) == 0 {
+		buckets = defaultBuckets
+	}
+
+	latency := prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Namespace:   name,
+		Subsystem:   "http",
+		Name:        latencyName,
+		Help:        "How long it took to process the request, partitioned by status code, method and HTTP path.",
+		ConstLabels: prometheus.Labels{"service": name},
+		Buckets:     buckets,
+	},
+		[]string{"code", "method"},
+	)
+	prometheus.MustRegister(latency)
+
+	f := func(h http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			ww := mw.NewWrapResponseWriter(w, r.ProtoMajor)
+			h.ServeHTTP(ww, r)
+			requests.WithLabelValues(http.StatusText(ww.Status()), r.Method).Inc()
+			latency.WithLabelValues(http.StatusText(ww.Status()), r.Method).Observe(float64(time.Since(start).Nanoseconds()) / 1e6) // milliseconds
+		}
+		return http.HandlerFunc(fn)
+	}
+	return f
+}
+
+// PrometheusStats returns a new Prometheus middleware handler.
+func PrometheusDetailedStats(name string, buckets ...float64) func(http.Handler) http.Handler {
+
+	prometheus.Unregister(prometheus.NewProcessCollector(os.Getpid(), ""))
+	prometheus.Unregister(prometheus.NewGoCollector())
+	prometheus.MustRegister(prometheus.NewProcessCollector(os.Getpid(), name))
+	prometheus.MustRegister(prometheus.NewGoCollector())
+
 	requests := prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Namespace:   name,
