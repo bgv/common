27fe9af9911abe4ed854af2a185798b096f375ee
Boris V <boris@codechimp.io>
2018-04-03 15:47:27 +0100

    Slight refactoring

--- middleware/servername.go
+++ middleware/servername.go
@@ -5,13 +5,12 @@ import (
 )
 
 // ServerName exports servername header to all requests
-func ServerName(name string) func(http.Handler) http.Handler {
-	f := func(h http.Handler) http.Handler {
+func ServerName(name string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Server", name)
-			h.ServeHTTP(w, r)
+			next.ServeHTTP(w, r)
 		}
 		return http.HandlerFunc(fn)
 	}
-	return f
 }
